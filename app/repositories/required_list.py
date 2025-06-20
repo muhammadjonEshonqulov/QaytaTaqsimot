8b8f76e7375d8a5c86e495d203b07900e0211cfc
muhammadjon <muhammadjon4464@gmail.com>
2025-06-20 16:48:00 +0500

    deadline added to me api

--- app/repositories/required_list.py
+++ app/repositories/required_list.py
@@ -6,18 +6,22 @@ from app.models.required_list import RequiredList
 def seed_required_list(db: Session):
     """RequiredList jadvaliga default qiymatlarni qo‘shish."""
     default_entries = [
-        "Kitobxonlik madaniyati",
-        "\"5 muhim tashabbus\" doirasidagi ishtiroki",
-        "Talabaning akademik ta’riflanishi",
-        "Talaba xulq-atvori, ichki tartib qoidalari va odob-axloq kodeksiga rioya qilishi",
-        "Xalqaro, respublika miqyosidagi olimpiadalar va sport musobaqalarida erishgan natijalari",
-        "Talabaning darsga to‘liq va kechikmasdan kelishi",
-        "Talabalarning \"Murabbiylar soati\"dagi faolligi",
-        "Talabaning iqtidorli yoshlar toifasidagi faol ishtiroki",
-        "Volontyorlik yarmarkasi ishtirokchisi",
-        "Teatr va muzey, xiyobon, kino tomosha qilish, sport bilan shug‘ullanishi va sog‘lom turmush tarziga amal qilishi",
-        "Ma’naviy-ma’rifiy sohaga oid boshqa yo‘nalishlardagi faolligi"
+        "1. Kitobxonlik madaniyati (0-20)"
+        "2. \"5 muhim tashabbus\" doirasidagi ishtiroki (0-20)"
+        "3. Talabaning akademik ta’riflanishi (0-10)"
+        "4. Talaba xulq-atvori, ichki tartib qoidalari va odob-axloq kodeksiga rioya qilishi (0-5)"
+        "5. Xalqaro, respublika miqyosidagi olimpiadalar va sport musobaqalarida erishgan natijalari (0-10)"
+        "6. Talabaning darsga to‘liq va kechikmasdan kelishi (0-5)"
+        "7. Talabalarning \"Murabbiylar soati\"dagi faolligi (0-10)"
+        "8. Talabaning iqtidorli yoshlar toifasidagi faol ishtiroki (0-5)"
+        "9. Volontyorlik yarmarkasi ishtirokchisi (0-5)"
+        "10. Teatr va muzey, xiyobon, kino tomosha qilish, sport bilan shug‘ullanishi va sog‘lom turmush tarziga amal qilishi (0-5)"
+        "11. Ma’naviy-ma’rifiy sohaga oid boshqa yo‘nalishlardagi faolligi (0-5)"
     ]
+
+
+
+
 
     for entry in default_entries:
         exists = db.query(RequiredList).filter_by(name=entry).first()
