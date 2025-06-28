2284b237257d3a0f4137bb2cf9eefe89538de721
muhammadjon <muhammadjon4464@gmail.com>
2025-06-28 11:44:28 +0500

    transkript yuklanmagan bo'lsa students qaytmaydi

--- app/repositories/required_list.py
+++ app/repositories/required_list.py
@@ -8,14 +8,14 @@ def seed_required_list(db: Session):
     default_entries = [
         "1. Kitobxonlik madaniyati (0-20)",
         "2. \"5 muhim tashabbus\" doirasidagi ishtiroki (0-20)",
-        "3. Talabaning akademik ta’riflanishi (0-10)",
-        "4. Talaba xulq-atvori, ichki tartib qoidalari va odob-axloq kodeksiga rioya qilishi (0-5)",
-        "5. Xalqaro, respublika miqyosidagi olimpiadalar va sport musobaqalarida erishgan natijalari (0-10)",
-        "6. Talabaning darsga to‘liq va kechikmasdan kelishi (0-5)",
-        "7. Talabalarning \"Murabbiylar soati\"dagi faolligi (0-10)",
-        "8. Talabaning iqtidorli yoshlar toifasidagi faol ishtiroki (0-5)",
-        "9. Volontyorlik yarmarkasi ishtirokchisi (0-5)",
-        "10. Teatr va muzey, xiyobon, kino tomosha qilish, sport bilan shug‘ullanishi va sog‘lom turmush tarziga amal qilishi (0-5)",
+        "3. Talabaning akademik o'zlashtirishi (0-10)",
+        "4. Talabaning oliy ta'lim tashkilotining ichki tartib qoidalari va Odob-axloq kodeksiga rioya etishi (0-5)",
+        "5. Xalqaro, respublika, viloyat miqyosidagi ko'rik-tanlov, fan olimpiadalari va sport musobaqalarida erishgan natijalari (0-10)",
+        "6. Talabaning darslarga to‘liq va kechikmasdan kelishi (0-5)",
+        "7. Talabalarning \"Ma'rifat darslari\"dagi faol ishtiroki (0-10)",
+        "8. Volontyorlik va jamoat ishlaridagi faolligi (0-5)",
+        "9. Teatr va muzey, xiyobon, kino, tarixiy qadamjolarga tashriflar (0-5)",
+        "10. Talabalarning sport bilan shug‘ullanishi va sog'lom turmush tarziga amal qilishi (0-5)",
         "11. Ma’naviy-ma’rifiy sohaga oid boshqa yo‘nalishlardagi faolligi (0-5)",
     ]
 
